0447331c23b0cfb13598e190c14c7c3ac07b8823
Aung Thu Zaw ( Xyder ) <105932994+aung-thu-zaw@users.noreply.github.com>
2024-04-18 15:26:28 +0630

    implement flowbite and tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/flowbite/**/*.js",
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                roboto: ["Roboto", "sans-serif"],
+                poppins: ["Poppins", "sans-serif"],
+            },
+            screens: {
+                sm: "640px",
+                md: "768px",
+                lg: "1024px",
+                xl: "1280px",
+            },
+        },
+    },
+    plugins: [require("flowbite/plugin")],
+};
