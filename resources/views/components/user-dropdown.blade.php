5e80d0957f0a1cb6aa877233162c069c367e97e4
Aung Thu Zaw ( Xyder ) <105932994+aung-thu-zaw@users.noreply.github.com>
2024-04-22 09:00:15 +0630

    change user dropdown style

--- resources/views/components/user-dropdown.blade.php
+++ resources/views/components/user-dropdown.blade.php
@@ -1,12 +1,55 @@
-<x-dropdown class="">
-    <x-slot:trigger class="flex items-center space-x-3">
-        <x-avatar
-            image="https://img.freepik.com/free-psd/3d-illustration-person-with-glasses_23-2149436185.jpg?size=626&ext=jpg"
+<div class="dropdown">
+    <div tabindex="0" role="button" class="flex items-center space-x-3 cursor-pointer">
+        <x-avatar image="
+        https://img.freepik.com/free-psd/3d-illustration-person-with-glasses_23-2149436185.jpg?size=626&ext=jpg"
             class="w-10" />
 
         <h3 class="text-sm font-bold hidden md:inline-block">Aung Thu Zaw</h3>
-    </x-slot:trigger>
+    </div>
+    <ul tabindex="0"
+        class="dropdown-content z-[1] menu p-2 shadow w-48 rounded-md  bg-primaryBackground border border-neutral-700 font-semibold text-sm">
+        <li>
+            <a href="#" class="hover:bg-secondaryBackground space-x-1.5">
+                <i class="fa-solid fa-user-circle"></i>
+                <span>
+                    My Profile
+                </span>
+            </a>
+        </li>
 
-    <x-menu-item title="Archive" />
-    <x-menu-item title="Move" />
-</x-dropdown>
+        <li>
+            <a href="#" class="hover:bg-secondaryBackground space-x-1.5">
+                <i class="fa-solid fa-gear"></i>
+                <span>
+                    Account Settings
+                </span>
+            </a>
+        </li>
+        <li>
+            <a href="#" class="hover:bg-secondaryBackground space-x-1.5">
+                <i class="fa-solid fa-heart"></i>
+                <span>
+                    My Wishlists
+                </span>
+            </a>
+        </li>
+
+        <li>
+            <a href="#" class="hover:bg-secondaryBackground space-x-1.5">
+                <i class="fa-solid fa-star"></i>
+                <span>
+                    Rating & Reviews
+                </span>
+            </a>
+        </li>
+
+        <li>
+            <a href="#" class="hover:bg-secondaryBackground space-x-1.5">
+                <i class="fa-solid fa-right-to-bracket"></i>
+                <span>
+                    Logout
+                </span>
+            </a>
+        </li>
+    </ul>
+</div>
